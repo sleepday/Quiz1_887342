4bcbe49e00d3fb9d6808a445517a21622b1374cd
chinnawat sinnarai <chinnawat.sinnarai@gmail.com>
2016-09-17 15:11:24 +0700

    Quiz 1

---
+++ quiz1/main.rb
@@ -0,0 +1,75 @@
+US_states = 
+
+	{"Alabama" => "AL",
+	"Alaska" => "AK",
+	"Arizona" => "AZ",
+	"Arkansas" => "AR",
+	"California" => "CA",
+	"Colorado" => "CO",
+	"Connecticut" => "CT",
+	"Delaware" => "DE",
+	"District of Columbia" => "DC",
+	"Florida" => "FL",
+	"Georgia" => "GA",
+	"Hawaii" => "HI",
+	"Idaho" => "ID",
+	"Illinois" => "IL",
+	"Indiana" => "IN",
+	"Iowa" => "IA",
+	"Kansas" => "KS",
+	"Kentucky" => "KY",
+	"Louisiana" => "LA",
+	"Maine" => "ME",
+	"Maryland" => "MD",
+	"Massachusetts" => "MA",
+	"Michigan" => "MI",
+	"Minnesota" => "MN",
+	"Mississippi" => "MS",
+	"Missouri" => "MO",
+	"Montana" => "MT",
+	"Nebraska" => "NE",
+	"Nevada" => "NV",
+	"New Hampshire" => "NH",
+	"New Jersey" => "NJ",
+	"New Mexico" => "NM",
+	"New York" => "NY",
+	"North Carolina" => "NC",
+	"North Dakota" => "ND",
+	"Ohio" => "OH",
+	"Oklahoma" => "OK",
+	"Oregon" => "OR",
+	"Pennsylvania" => "PA",
+	"Rhode Island" => "RI",
+	"South Carolina" => "SC",
+	"South Dakota" => "SD",
+	"Tennessee" => "TN",
+	"Texas" => "TX",
+	"Utah" => "UT",
+	"Vermont" => "VT",
+	"Virginia" => "VA",
+	"Washington" => "WA",
+	"West Virginia" => "WV",
+	"Wisconsin" => "WI",
+"Wyoming" => "WY"}
+
+
+   
+
+  US_states.each {|key,value|
+  num = value.length-1
+      if(value[num]=="T"||value[num]=="N")
+          puts value
+      end
+  }
+
+
+
+
+puts US_states.sort
+
+  US_states.each {|key,value|
+  num = key.length-1
+     if((key[0]=="A"&&key[-1]=="a")||(key[0]=="E"&&key[-1]=="E")||(key[0]=="I"&&key[-1]=="i")||(key[0]=="O"&&key[-1]=="o")||(key[0]=="U"&&key[-1]=="u"))
+          puts key
+      end
+  } 
